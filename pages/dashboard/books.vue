a9904d3ad0244164bd31de7c6f65907089741106
cecep <cecepjanuardi31@gmail.com>
2022-12-07 00:59:21 +0700

    datafetching

--- pages/dashboard/books.vue
+++ pages/dashboard/books.vue
@@ -1,33 +1,38 @@
 <template>
-    <ol v-for="book in books">
-        <li >{{book.title}}</li>
+    <ol class="list-decimal" v-for="book in books">
+        <li>{{ book.title }}|{{book.desc}}</li>
     </ol>
-  
+
 </template>
 <script setup>
-import axios from 'axios';
-
-let books;
 let token = useCookie("token");
 
-
-onMounted(() => {
-
-    var config = {
-        method: 'get',
-        url: 'https://api.pilput.my.id/api/v1/books',
-        headers: {
-            'Authorization': `Bearer ${token.value}`
-        }
-    };
-
-    axios(config)
-        .then(function (response) {
-            books = response.data
-            console.log(books);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+const {data} = await useFetch("https://api.pilput.my.id/api/v1/books", {
+    headers: { 
+        Authorization: `Bearer ${token.value}`, 
+        method: "get"
+    }
 });
+const books = data
+
+
+// onMounted(() => {
+
+//     var config = {
+//         method: 'get',
+//         url: 'https://api.pilput.my.id/api/v1/books',
+//         headers: {
+//             'Authorization': `Bearer ${token.value}`
+//         }
+//     };
+
+//     axios(config)
+//         .then(function (response) {
+//             books = response.data
+//             console.log(books);
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         });
+// });
 </script>
